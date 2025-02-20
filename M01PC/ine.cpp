b1ba82339deaa1df3d98bf0914565c4151afcd90
RDK <photocromatic@gmail.com>
2025-02-19 21:06:46 -0600

    EDC PC

--- M01PC/ine.cpp
+++ M01PC/ine.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
 	cout << "CPP App" << endl; // Sentence
 
-	// ine number, district number
+	// INE number, District number
 	int ine_number = 234;
 	int district_number = 78;
 
-	// Valite INE & District
+	// Validate INE number & District number
 	if( ine_number > 0 && district_number == 20 ) {
-		cout << "Puede votar" << endl;
+		cout << "Puedes votar" << endl;
 		cout << "Has votado" << endl;
 		cout << "Te han marcado el pulgar" << endl;
 	}
