5e4a97cf5563233c19046b1cb3a1edd51b9e8e99
G-Moll <photocromatic@gmail.com>
2025-02-26 15:50:20 -0600

    EDC PC

---
+++ M01PC/CHALLS/Chall01-Cursos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+    /*
+        Se ofrecen cursos de
+        Dibujo y Computación
+        para niños.
+        
+        El rango de edades es
+        de los 7 hasta los 12 años.
+        
+        Los niños de 7 a 10 años
+        sólo pueden inscribirse
+        en el curso de dibujo,
+        los demás niños sólo
+        pueden inscribirse
+        al curso de computación.
+    */
+int main() {
+
+
+    return 0;
+}
