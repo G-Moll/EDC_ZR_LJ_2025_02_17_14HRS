94ca67d6a0e6c3f533bd33118a5e5af1e90c8e82
G-Moll <photocromatic@gmail.com>
2025-03-06 15:34:47 -0600

    EDC PC

--- M01PC/S02/Lab05-Structures/Structures.cpp
+++ M01PC/S02/Lab05-Structures/Structures.cpp
@@ -16,21 +16,24 @@ int main() {
 	joshua.lastname = "Hernandez";
 	joshua.age = 33;
 	joshua.registered = true;
+	
 	cout <<
-		joshua.name <<
-		joshua.lastname <<
-		joshua.age <<
+		joshua.name << " " <<
+		joshua.lastname << " " <<
+		joshua.age << " " <<
 		joshua.registered << endl;
+
 
 	Student john;
 	john.name = "John";
 	john.lastname = "Perez";
 	john.age = 40;
 	john.registered = false;
+
 	cout <<
-		john.name <<
-		john.lastname <<
-		john.age <<
+		john.name << " " <<
+		john.lastname << " " <<
+		john.age << " " <<
 		john.registered << endl;
 	
     return 0;
