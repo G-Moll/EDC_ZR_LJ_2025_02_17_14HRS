2e73a863e7c57bf9f9190ff8ad0e7bde985c9a15
G-Moll <photocromatic@gmail.com>
2025-06-05 16:34:19 -0600

    EDC PY

---
+++ M04PY/S03/LAB-04.00-collections.py
@@ -0,0 +1,24 @@
+# Diccionario
+student = {
+    "name": "Joshua",
+    "notes": [ 10, 8, 6, 7, 2.8]
+}
+print( student )
+
+# Lista que contiene Diccionarios
+group = [
+    { "name": "Joshua", "notes": [ 1 ]  },
+    { "name": "Martha", "notes": [ 2, 5 , 5 ]  },
+    { "name": "Pedro", "notes": [ 10, 0 ]  }
+]
+print( group )
+
+# Lista
+some_list = [ 10, 20, 80, "a", "b", "c" ]
+print( some_list )
+
+# Diccionarios que contienen distintos tipos de datos
+dict_one = { "a": 10, "b": True, "c": [], "d": {}  }
+dict_two = { "a": 1, "b": False, "c": [], "d": {}  }
+print( dict_one, dict_two )
+
---
+++ M04PY/S03/LAB-04.04-students-group.py
@@ -0,0 +1,11 @@
+- Guardar nombres de alumnos de un grupo
+    - Guardar notas que ha tenido cada alumno
+    - Cada alumno puede tener distinta cantidad de notas
+    - La información debe ser guardadada en un Diccionario
+        - Las llaves(/claves/keys) son los nombres de los alumnos
+        - los valores serán las notas de cada alumno
+    - El programa debe mostrar
+        - La lista de alumnos
+        - el promedio de cada uno de ellos
+
+    - Considerar cuando ya existe un alumno con cierto nombre dentro del grupo
