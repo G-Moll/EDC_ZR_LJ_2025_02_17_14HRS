06bb9121fe163726d04390fad026ede87ce8b3cb
G-Moll <photocromatic@gmail.com>
2025-06-12 16:30:25 -0600

    EDC PY

--- M04PY/S03/LAB-04.04-students-group.py
+++ M04PY/S03/LAB-04.04-students-group.py
@@ -18,7 +18,10 @@ print( "INFORMACIÓN DEL GRUPO" )
 for current_student in students_group:
     current_notes = students_group[ current_student ]
     current_quantity_notes = len( current_notes )
-    message = f"\tNombre: { current_student } \n\tNúm Notas: { current_quantity_notes } \n\tPromedio { sum( current_notes ) / current_quantity_notes } \n\tCalificaciones: { current_notes }\n"
+    current_avg = sum( current_notes ) / current_quantity_notes
+    current_max = max( current_notes )
+    current_min = min( current_notes )
+    message = f"\tNombre: { current_student } \n\tNúm Notas: { current_quantity_notes } \n\tPromedio { current_avg } \n\tMax { current_max } \n\tMin { current_min } \n\tCalificaciones: { current_notes }\n"
     print( message )
 
 print( students_group )
