174ee74a4c39500a325918fa5cae155500915ca7
G-Moll <photocromatic@gmail.com>
2025-06-18 15:55:09 -0600

    EDC PY

--- M04PY/S04/LAB-06.03-product-payment.py
+++ M04PY/S04/LAB-06.03-product-payment.py
@@ -9,3 +9,17 @@
 #         # Realizar un algoritmo para determinar
 #         # cuánto debe pagar mensualmente
 #         # y el total de lo que pagó después de los 20 meses.
+
+base_payment = 10
+current_payment = 0
+total_payment = 0
+
+for n in range( 1, 21 ):
+    if n == 1:
+        current_payment = base_payment
+    else:
+        current_payment = current_payment * 2
+    total_payment += current_payment
+    print( f"El pago del mes { n } es: ",  current_payment )
+
+print( f"El pago total es { total_payment }")
--- M04PY/S04/LAB-06.04-factorial-numbers.py
+++ M04PY/S04/LAB-06.04-factorial-numbers.py
@@ -8,3 +8,17 @@
 #         # de todos los enteros entre 1 y el propio número
 #         # y se representa por el número seguido de un signo de exclamación.
 #         # Por ejemplo 5! = 1x2x3x4x5=120).
+
+factorial_input = int( input( "Captura un número para calcular su factorial: " ) )
+factorial_result = 0
+current_calculation = 1
+
+if factorial_input > 0:
+    for n in range( 1, factorial_input + 1 ):
+        current_calculation = current_calculation * n
+    print( f"El factorial de { factorial_input } es { current_calculation }" )
+else:
+    print( "El factorial de cero es 1" )
+
+
+
