b6541cb64fde61bb8da91f6b3bb28ad9d4cad1dd
G-Moll <photocromatic@gmail.com>
2025-06-26 16:11:45 -0600

    EDC PY

--- M04PY/S05/LAB-08.02-string-occurrences.py
+++ M04PY/S05/LAB-08.02-string-occurrences.py
@@ -2,7 +2,30 @@
 
 # Ejercicio 0802 (2 puntos)
 #     - Escribe un programa
-#         - que lea una cadena
-#         - y devuelva un diccionario
+#         - que lea una cadena (input)
+#         - y devuelva un diccionario ({})
 #             - con la cantidad de apariciones
 #             - de cada carácter en la cadena.
+
+# "Hello"
+# print( { "H": 1, "e": 1, "l": 2, "o": 1 } )
+# Caracter "H", Conteo 1
+# Caracter "e", Conteo 1
+# Caracter "l", Conteo 2
+
+phrase_chars = {}
+input_string = input( "Captura una frase: " )
+# input_string = "Lorem ipsum Lorem ipsum ipsum ipsum 123 123 123 @...."
+
+for c in range( len( input_string ) ):
+    if not phrase_chars.get( input_string[ c ] ):
+        phrase_chars.update( { input_string[ c ]: 1 } )
+    else:
+        current_key = input_string[ c ]
+        updated_value = phrase_chars.get( current_key ) + 1
+        phrase_chars[ current_key ] = updated_value
+
+for c in phrase_chars:
+    print( f'Caracter { c }, Conteo { phrase_chars[ c ] }'  )
+
+# print( phrase_chars )
--- M04PY/S05/LAB-08.03-fruits-prices.py
+++ M04PY/S05/LAB-08.03-fruits-prices.py
@@ -13,3 +13,37 @@
 #         # Si la fruta no existe nos dará un error.
 #         # Tras cada consulta
 #             # el programa nos preguntará si queremos hacer otra consulta.
+
+fruits_data = {
+    "manzana": 40,
+    "platano": 20,
+    "moras": 30,
+    "cereza": 50,
+    "kiwi": 40,
+    "mango": 25,
+    "melon": 20
+}
+print( fruits_data )
+
+while 1 == 1:
+
+    fruit_choice = input( "Qué fruta quieres? " ) # manzana
+    fruit_quantity: int
+    
+    if fruit_choice not in fruits_data:
+        print( "La fruta seleccionada no existe... " )
+        continue
+    else:
+        fruit_quantity = int( input( "Cuanto quieres comprar? " ) )
+        fruit_name = fruit_choice
+        fruit_price = fruits_data[ fruit_choice ]
+        fruit_total = fruit_price * fruit_quantity
+        print( f"Fruta { fruit_name }, Precio { fruit_price } Compra Kg{ fruit_quantity }, Total { fruit_total }" )
+
+    new_request = int( input( "Quieres hacer otra compra: (0) Salir, (1) Otra compra: " ) )
+
+    if new_request == 0:
+        print( "Gracias por tu compra." )
+        break
+    elif new_request == 1:
+        continue
