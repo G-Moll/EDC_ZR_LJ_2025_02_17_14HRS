671daa086c1349ad52e9d2f5767472b832f0fb86
G-Moll <photocromatic@gmail.com>
2025-06-30 16:01:55 -0600

    EDC PY

--- M04PY/S04/LAB_0701_person_class.py
+++ M04PY/S04/LAB_0701_person_class.py
@@ -2,7 +2,7 @@
 
 # Ejercicio 0701 (2 puntos)
 #     - Vamos a crear una clase llamada Persona.
-#         - Sus atributos son:
+#         - Sus atributos (propiedades) son:
 #             - nombre,
 #             - edad
 #             - y DNI.
@@ -14,3 +14,18 @@
 #                 - Muestra los datos de la persona.
 #             - esMayorDeEdad():
 #                 - Devuelve un valor lógico indicando si es mayor de edad
+
+class Person:
+    name: str
+    age: int
+    rfc: str
+
+    def __init__( self, name, age, rfc ):
+        self.name = name
+        self.age = age
+        self.rfc = rfc
+        print( f"Hola soy { self.name } { self.rfc }, tengo { self.age } años" )
+
+personOne = Person( "Joshua", 30, "ABC123" )
+personTwo = Person( "Luke", 25, "XYZ456" )
+personBis = Person( "John", 18, "NML147" )
